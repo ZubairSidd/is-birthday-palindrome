d2e47be7c09289a96c6ec22000d88aee76e1c451
Zubair Siddique <zubairsidd20@gmail.com>
2021-09-13 15:10:21 +0530

    logic and html completed

--- app.js
+++ app.js
@@ -3,26 +3,12 @@ const checkButton = document.querySelector("#checkButton");
 
 const answer = document.querySelector("#answer");
 
-// const checkButtonHandler = () => {
-//   const bdValue = dateInput.value.split("-").join("");
-//   const bdValueReverse = bdValue.split("").reverse().join("");
-//   answer.innerText = `${bdValue} ${bdValueReverse}`;
-// };
-
-// checkButton.addEventListener("click", checkButtonHandler);
-
 const reverseStr = (str) => {
   return str.split("").reverse().join("");
 };
 
 const isPalindrome = (str) => {
   return str === reverseStr(str);
-};
-
-const dt = {
-  day: 2,
-  month: 11,
-  year: 2020,
 };
 
 const dateToString = (date) => {
@@ -61,3 +47,80 @@ const checkPalindromeAll = (date) => {
   }
   return output;
 };
+
+const isLeapYear = (year) => {
+  if (year % 400 === 0) return true;
+  if (year % 100 === 0) return false;
+  if (year % 4 === 0) return true;
+  return false;
+};
+
+const getNextDate = (date) => {
+  let { day, month, year } = date;
+  day = day + 1;
+
+  const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  if (month === 2) {
+    if (isLeapYear(year)) {
+      if (day > 29) {
+        day = 1;
+        month++;
+      }
+    } else {
+      if (day > 28) {
+        day = 1;
+        month++;
+      }
+    }
+  } else {
+    if (day > daysInMonth[month - 1]) {
+      day = 1;
+      month++;
+    }
+  }
+
+  if (month > 12) {
+    month = 1;
+    year++;
+  }
+
+  return { day, month, year };
+};
+
+const getNextPalindrome = (date) => {
+  let dayCount = 0;
+  let nextDate = getNextDate(date);
+
+  while (true) {
+    dayCount++;
+    if (checkPalindromeAll(nextDate)) break;
+    nextDate = getNextDate(nextDate);
+  }
+  return { dayCount, nextDate };
+};
+
+const dt = {
+  day: "31",
+  month: "12",
+  year: "2021",
+};
+
+const checkButtonHandler = (e) => {
+  const bdValue = dateInput.value.split("-");
+  const date = {
+    day: Number(bdValue[2]),
+    month: Number(bdValue[1]),
+    year: Number(bdValue[0]),
+  };
+
+  if (checkPalindromeAll(date)) {
+    answer.innerText = `Your Birthday is Palindrome`;
+  } else {
+    const { dayCount, nextDate } = getNextPalindrome(date);
+    answer.innerText = `You missed Palindrome by ${dayCount} days and date is ${nextDate.day}-${nextDate.month}-${nextDate.year}`;
+  }
+  // console.log(getNextPalindrome(dt));
+};
+
+checkButton.addEventListener("click", checkButtonHandler);
